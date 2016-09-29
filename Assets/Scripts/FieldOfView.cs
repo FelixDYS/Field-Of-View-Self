fd0ce1966e507b79820de80b6600071c76a46e21
Felix <felix@angellecho.com>
2016-09-29 19:07:16 +0800

    first

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    [HideInInspector]
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    public float meshResolution;
+
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", 0.2f);
+    }
+
+    void Update()
+    {
+        DrawFieldOfView();    
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets(); 
+        }
+    }
+
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+
+        Collider[] targetInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetInViewRadius.Length; i++)
+        {
+            Transform target = targetInViewRadius[i].transform;
+            target.GetComponent<MeshRenderer>().material.color = new Color(0, 0.333f, 0.882f);
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                    target.GetComponent<MeshRenderer>().material.color = Color.red;
+                }
+            }
+        }
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+
+        for (int i = 0; i < stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            Debug.DrawLine(transform.position, transform.position + DirFromAngel(angle, true) * viewRadius, Color.red);
+        
+        }
+
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngel(globalAngle, true);
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle); 
+        }
+    }
+
+    public Vector3 DirFromAngel(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dst;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dst, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dst = _dst;
+            angle = _angle;
+        }
+    }
+}
